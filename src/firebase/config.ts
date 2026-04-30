938111f83c7c0def774d5bf6d93435d6829b5f9c
معتصم الغزالي <moutasm.alghazali@gmail.com>
2026-04-30 19:03:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3347269647-e7dda",
+  "appId": "1:406139599207:web:6d31a2e5cbe8865b2057c2",
+  "apiKey": "AIzaSyBlM6mSGo4JksNrv9ij8K9e6ejzHjRdb3M",
+  "authDomain": "studio-3347269647-e7dda.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "406139599207"
+};
